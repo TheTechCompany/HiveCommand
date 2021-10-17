b2df0f1a08e8c0771b11bc1791d71d173b72e994
Ross Leitch <professional.balbatross@gmail.com>
2021-10-17 19:12:59 +1300

    Testing opcua-server

---
+++ packages/clients/revpi/src/io-bus/ValueBank.ts
@@ -0,0 +1,26 @@
+export class ValueBank { 
+
+	private values: {[key: string]: {port: string, value: any}[]} = {};
+
+	constructor(){
+
+	}
+
+	get(id: string, port: string){
+		return this.values[id].find((a) => a.port == port)?.value;
+	}
+
+	setMany(id: string, values: {port: string, value: string}[]){
+		values.forEach((value) => {
+			this.set(id, value.port, value.value)
+		})
+	}
+
+	set(id: string, port: string, value: any){
+		let bus = this.values[id]
+		let ix = bus.map((x) => x.port).indexOf(port)
+		if(ix > -1){
+			this.values[id][ix].value= value;
+		}
+	}
+}
