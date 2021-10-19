3b1227e93e5cc4519ab6b3f2b6dc89a2652d8148
Ross Leitch <professional.balbatross@gmail.com>
2021-10-20 01:49:40 +1300

    Big push to try get DP1213 working

--- packages/core/command-network/src/index.ts
+++ packages/core/command-network/src/index.ts
@@ -178,7 +178,7 @@ export class CommandNetwork {
 									get: () => {
 										let value = this.valueBank.get?.(bus.id, `${device.ix + 1}`)
 
-										return new Variant({dataType: DataType.Double, value: value?.[output.name]})
+										return new Variant({dataType: DataType.Double, value: value?.[`${output.name}-${output.subindex}`]})
 									},
 									set: (value: Variant) => {
 										
