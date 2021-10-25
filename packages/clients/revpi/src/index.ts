ee15a2ad85fd99ca85821ad179d25ee3ae2ef426
Ross Leitch <professional.balbatross@gmail.com>
2021-10-25 17:27:40 +1300

    Send device descriptions for RevPI

--- packages/clients/revpi/src/index.ts
+++ packages/clients/revpi/src/index.ts
@@ -170,7 +170,6 @@ export class CommandClient {
 
 			//TODO DEDUPE this
 			plugin?.on('PORT:VALUE', (event) => {
-				console.log("Port Value", event)
 				this.valueBank.set(event.bus, event.port, event.value)
 			})
 		}))
