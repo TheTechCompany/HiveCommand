3d1cde476ad45ce06fd21bd83b79bf6952dc63a2
Ross Leitch <professional.balbatross@gmail.com>
2021-10-26 18:56:51 +1300

    Restructure loadMachine

--- packages/clients/revpi/src/index.ts
+++ packages/clients/revpi/src/index.ts
@@ -111,7 +111,8 @@ export class CommandClient {
 	//Request state + translator for name
 	async requestOperation(event: {device: string, operation: string}){
 		console.log("Requesting operation with device name - StateMachine")
-		let busPort = this.portAssignment.find((a) => a.id == event.device)
+		let busPort = this.portAssignment.find((a) => a.name == event.device)
+		
 		if(!busPort?.bus || !busPort.port) return new Error("No bus-port found");
 
 		let action = busPort.actions?.find((a) => a.key == event.operation)
