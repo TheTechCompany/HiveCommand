1227af561a8b1885df92120eb58c6802b80e888f
Ross Leitch <professional.balbatross@gmail.com>
2021-10-30 14:23:19 +1300

    Change timer to 1000

---
+++ packages/clients/revpi/src/io-bus/DeviceMap.ts
@@ -0,0 +1,74 @@
+import { AssignmentPayload } from "@hive-command/network";
+import PIDController from "node-pid-controller";
+
+export class DeviceMap {
+	private assignment: AssignmentPayload[];
+
+	constructor(assignment?: AssignmentPayload[]){
+		this.assignment = assignment = [];
+	}
+
+	public setAssignment(assignment: AssignmentPayload[]){
+		this.assignment = assignment
+	}
+
+	//Init plugin by device id
+	public async setupDevicePlugins(deviceId: string){
+		const device = this.getDeviceById(deviceId)
+
+		await Promise.all((device?.plugins || []).map(async (plugin, ix) => {
+
+			let pluginObject = plugin.configuration.reduce<{
+				p?: string,
+				i?: string,
+				d?: string,
+				target?: string;
+			}>((prev, curr) => ({...prev, [curr.key]: curr.value}), {})
+
+			const portIx = this.getDeviceIndexById(deviceId)
+
+			if(portIx > -1){
+				let plugins = this.assignment[portIx].plugins?.slice() || [];
+
+				plugins[ix].instance = new PIDController({
+					k_p: pluginObject.p ? parseFloat(pluginObject.p) : 0.5,
+					k_i: pluginObject.i ? parseFloat(pluginObject.i) : 0.01,
+					k_d: pluginObject.d ? parseFloat(pluginObject.d) : 0.01,
+					dt: 1
+				})
+
+				plugins[ix].instance.setTarget(pluginObject.target ? parseFloat(pluginObject.target) : 0)
+
+				this.assignment[portIx].plugins = plugins
+			}
+
+			console.log("PLugin init ", plugin.name, pluginObject)
+		}))
+	}
+
+	//Get device index
+	public getDeviceIndexById(id: string): number {
+		return this.assignment.map((x) => x.id).indexOf(id);;
+	}
+
+	//Get devices with plugins
+	public getDevicesWithPlugins(): AssignmentPayload[] {
+		return this.assignment.filter(assignment => assignment.plugins !== undefined && assignment.plugins.length > 0);
+	}
+
+	//Get device name from assignment by bus and port
+	public getDeviceName(bus: string, port: string): string | undefined {
+		return this.assignment.find(assignment => assignment.bus === bus && assignment.port === port)?.name;
+	}
+
+	//Get device by id
+	public getDeviceById(id: string): AssignmentPayload | undefined {
+		return this.assignment.find((a) => a.id == id)
+	}
+
+	//Get assignment by device name
+	public getDeviceBusPort(name: string): AssignmentPayload | undefined {
+		let device = this.assignment.find(assignment => assignment.name === name);
+		return device
+	}
+}
---
+++ packages/core/command-state-machine/src/State/index.ts
@@ -0,0 +1,31 @@
+export class State {
+
+	private internalState: {
+		[key: string]: {
+			[key: string]: any
+		}
+	};
+
+	constructor(state?: {[key: string]: any}) {
+		this.internalState = state || {};
+	}
+
+	public get(key: string): any {
+		return this.internalState[key]
+	}	
+
+	public getByKey(key: string, subKey: string): any {
+		return this.internalState[key][subKey];
+	}	
+
+	public update(key: string, value: any): void {
+		if(typeof(value) == "object"){
+			this.internalState[key] = {
+				...this.internalState[key],
+				...value
+			}
+		}else{
+			this.internalState[key] = value;
+		}
+	}
+}
