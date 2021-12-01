8854db758c591b993b63c1bcc68e3ec92df69716
Ross Leitch <professional.balbatross@gmail.com>
2021-12-02 11:14:19 +1300

    Init io-link client

--- packages/clients/revpi/src/plugins/IO-Link/index.ts
+++ packages/clients/revpi/src/plugins/IO-Link/index.ts
@@ -141,47 +141,47 @@ export default class IOLinkPlugin extends BasePlugin {
 		console.log(`IO-Link Discovering`)
 		const masters = await discoverMasters(this.networkInterface)	
 		console.log(`IO-Link Discovered ${masters.length} masters`)
-		
+
 		this.masters = await Promise.all(masters.map(async (master) => {
 			console.log("Discovering master")
 			const devices = await discoverDevices(master)
-			console.log("Discovered devices", devices)
+			console.log("Discovered devices", {devices})
 
 			const mappedDevices =  await Promise.all(devices.ports.map(async (port) => {
 				console.log({port})
 
-				let ioddDef = await this.ioddManager.lookupDevice(`${port.vendorId}:${port.deviceId}`)
-				if(!ioddDef) {
-					console.log("NO IODD", port)
-					return port;
-				}
-				let iodd = await this.ioddManager.getIODD(ioddDef?.iodd)
+				// let ioddDef = await this.ioddManager.lookupDevice(`${port.vendorId}:${port.deviceId}`)
+				// if(!ioddDef) {
+				// 	console.log("NO IODD", port)
+				// 	return port;
+				// }
+				// let iodd = await this.ioddManager.getIODD(ioddDef?.iodd)
 
 				return {
 					...port,
 					port: `${port.ix + 1}`,
-					iodd: iodd,
-					inputs: iodd?.function?.inputs?.map((x) => {
-							return x?.struct?.map((y) => {
-								return {
-									key: `${y.name}-${y.bits.subindex}`,
-									type: y?.bits?.type
-								}
-							})
-						}).reduce((prev, curr) => prev.concat(curr), [])
-					,
-					outputs: iodd?.function?.outputs?.map((x) => {
-						return x?.struct?.map((y) => {
-							return {
-								key: `${y.name}-${y.bits.subindex}`,
-								type: y?.bits?.type
-							}
-						})
-					}).reduce((prev, curr) => prev.concat(curr), [])
+					// iodd: iodd,
+					// inputs: iodd?.function?.inputs?.map((x) => {
+					// 		return x?.struct?.map((y) => {
+					// 			return {
+					// 				key: `${y.name}-${y.bits.subindex}`,
+					// 				type: y?.bits?.type
+					// 			}
+					// 		})
+					// 	}).reduce((prev, curr) => prev.concat(curr), [])
+					// ,
+					// outputs: iodd?.function?.outputs?.map((x) => {
+					// 	return x?.struct?.map((y) => {
+					// 		return {
+					// 			key: `${y.name}-${y.bits.subindex}`,
+					// 			type: y?.bits?.type
+					// 		}
+					// 	})
+					// }).reduce((prev, curr) => prev.concat(curr), [])
 				}
 			}))
 
-			console.log("Discovered devices", {devices})
+			console.log("Discovered devices", {mappedDevices})
 
 			// const iodd = await Promise.all(devices.unique.map(async (id) => {
 			// 	return await getIODD(this.ioddManager, id)
