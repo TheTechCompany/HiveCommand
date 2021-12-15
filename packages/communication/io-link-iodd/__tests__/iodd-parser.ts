59fa85639a2ac07391a7e3963a1412bcc998bb32
Ross Leitch <professional.balbatross@gmail.com>
2021-12-15 16:39:28 +1300

    Testing

--- packages/communication/io-link-iodd/__tests__/iodd-parser.ts
+++ packages/communication/io-link-iodd/__tests__/iodd-parser.ts
@@ -24,20 +24,20 @@ describe('IODD Parser', () => {
         expect(names[0].indexOf('Temperature') > -1).toBe(true)
     })
 
-    it('Filter IO-Link Raw Output', async () => {
+    // it('Filter IO-Link Raw Output', async () => {
        
-        let filters : IODDFilter[] = iodd_result.function.inputs.map((x) => {
-            return createFilter(x.struct.map((y) => ({name: y.name, ...y.bits})))
-        })
+    //     let filters : IODDFilter[] = iodd_result.function.inputs.map((x) => {
+    //         return createFilter(x.struct.map((y) => ({name: y.name, ...y.bits})))
+    //     })
 
-        filters.forEach((filter) => {
-           // let result = filter("00000200067CFE00") //pv8003 iodd.xml 0==
-            let result = filter('00000000FC00000000BCFF00')
-            console.log(result)
+    //     filters.forEach((filter) => {
+    //        // let result = filter("00000200067CFE00") //pv8003 iodd.xml 0==
+    //         let result = filter('00000000FC00000000BCFF00')
+    //         console.log(result)
 
-            expect(result.Temperature).toBe(188)
-        })
-    })
+    //         expect(result.Temperature).toBe(188)
+    //     })
+    // })
 
     it('IO-Link MDC', () => {
         let val = getBits('00000200067CFE00', 40, 8)
