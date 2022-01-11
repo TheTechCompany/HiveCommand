3b854c3e2e6aeb3793b2e472b9ee4756554e44d4
Ross Leitch <professional.balbatross@gmail.com>
2022-01-12 11:18:40 +1300

    stat

---
+++ packages/plugins/blessed-plugin/src/index.ts
@@ -0,0 +1,57 @@
+import { BasePlugin } from '@hive-command/plugin-base'
+import axios from 'axios';
+import log from 'loglevel';
+
+export default class BlessedPlugin extends BasePlugin {
+	public TAG : string = "BLESSED";
+
+	constructor(){
+		super()
+	}
+
+	async discover() : Promise<any[]> {
+		let port_template = {
+			product: 'output',
+			deviceId: 'blessed-do',
+			vendorId: 'blessed',
+			outputs: [{
+				key: 'active',
+				type: 'BooleanT'
+			}, {
+				key: 'value',
+				type: 'IntegerT'
+			}]
+		}
+
+		const data = await axios.get(`http://localhost:8765/layout`)
+		
+		let {cols, rows} = data.data
+
+		let devices = [];
+
+		for(var colIndex = 0; colIndex < cols; colIndex++){
+			for(var rowIndex = 0; rowIndex < rows; rowIndex++){
+				devices.push({
+					...port_template,
+					port: `O_${colIndex + 1}:${rowIndex + 1}`,
+					serial: `O_${colIndex + 1}:${rowIndex + 1}`,
+				})
+			}
+		}
+
+		return [{
+			id: `blessed`,
+			name: `Blessed Output`,
+			devices
+		}]
+	}
+
+	async write(bus: string | null, port: string, value: any){
+		log.info(`Writing ${value} to ${port}`)
+		await axios.post(`http://localhost:8765/update`, {
+			col: parseInt(port.split(':')[0]),
+			row: parseInt(port.split(':')[1]),
+			value
+		})
+	}
+}
