cc315cae9a74a1fdb637c809f8c72ebe390cf0c8
Ross Leitch <professional.balbatross@gmail.com>
2022-01-27 09:57:20 +1300

    Fixing up

--- packages/clients/local-client/src/plugins/PID.ts
+++ packages/clients/local-client/src/plugins/PID.ts
@@ -43,7 +43,7 @@ export default `
 
 			async start(){
 				this.running = true;
-				console.log({this})
+				console.log({thisvalue: this})
 				this.instance.setTarget(this.target)
 
 				(async () => {
