7cc33cc3f989ae442f1b38d3e491b36899304d0f
Ross Leitch <professional.balbatross@gmail.com>
2022-02-04 12:18:12 +1300

    Added remote-client cli

---
+++ packages/clients/remote-client/src/cli.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+import yargs from 'yargs/yargs';
+import { HiveRemoteClient } from ".";
+
+const argv = yargs(process.argv.slice(2)).options({
+  endpoint: { type: 'string', default: "hahei-flow.hexhive.io" },
+  deviceId: { type: 'string', required: true, description: 'CommandDevice ID' }
+}).argv;
+
+(async () => {
+	const { endpoint, deviceId } = await argv;
+
+	const client = new HiveRemoteClient({
+		endpoint: endpoint,
+		deviceId
+	})
+
+	await client.start()
+})
+
