e92f17ba9947d825e255de501ea5931c0c26d582
Ross Leitch <ross@hexhive.io>
2022-02-16 11:07:33 +1300

    Changing lifecycle

--- packages/core/command-state-machine/src/base-plugins/timer/index.ts
+++ packages/core/command-state-machine/src/base-plugins/timer/index.ts
@@ -10,17 +10,19 @@ export const handler = async (options: {timer: string}) => {
 	
 	// if(!isPrioritized || priority == this.node.id || priority == this.process.id){
 
-	console.time(`Timer ${id}`)
 		
 	let timer : Timer =  new Timer(timeout)
 
 	const timer_status = timer.countDown()
 
-	console.timeEnd(`Timer ${id}`)
 
 		
 	return {
-		promise: timer_status,
+		promise: async () => {
+			console.time(`Timer ${id}`)
+			await timer_status
+			console.timeEnd(`Timer ${id}`)
+		},
 		cancel: () => timer.stop()
 	}
 	// 	return timer_status;
