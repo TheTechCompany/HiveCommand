1eba74983050599df54953a0bc62434b473e0f97
Ross Leitch <ross@hexhive.io>
2022-05-11 20:20:40 +1200

    Discovery server

--- packages/network/hive-command-discovery-server/src/index.ts
+++ packages/network/hive-command-discovery-server/src/index.ts
@@ -122,8 +122,11 @@ export class DiscoveryServer {
     }
 
     async onSocketDisconnect(socket: Socket){
-        await this.data.updateLiveness((socket as any)?.networkName, false)
-        await this.data.updateMode((socket as any)?.networkName, 'disabled')
+        //TODO add graphql update
+        
+        // await this.data.updateLiveness((socket as any)?.networkName, false)
+        // await this.data.updateMode((socket as any)?.networkName, 'disabled')
+
         // await Device.updateOne({_id: (socket as any)?.machine_info.id}, {$set: {connected: false}})
         socket.removeAllListeners();
     }
