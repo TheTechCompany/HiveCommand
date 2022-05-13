7931cc9981798c6621b4d093b10da9f2c1fd2ed6
Ross Leitch <ross@hexhive.io>
2022-05-13 20:33:24 +1200

    run.sh sync-server

---
+++ packages/network/hive-command-discovery-server/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir -p /dev/net && \
+    mknod /dev/net/tun c 10 200 && \
+    chmod 600 /dev/net/tun
+
+openvpn --config /etc/openvpn/openvpn.conf &
+
+yarn start:prod --fqdn discovery.hexhive.io
