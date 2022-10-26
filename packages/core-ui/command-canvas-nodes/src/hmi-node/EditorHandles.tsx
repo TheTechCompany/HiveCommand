8de813134a66dd77b66a049c353ba10139921883
Ross Leitch <ross@hexhive.io>
2022-10-26 20:31:34 +1300

    Canvas nodes and pipe-path working again

--- packages/core-ui/command-canvas-nodes/src/hmi-node/EditorHandles.tsx
+++ packages/core-ui/command-canvas-nodes/src/hmi-node/EditorHandles.tsx
@@ -4,10 +4,33 @@ import React, { useContext, useRef } from 'react';
 import { ChangeCircle } from '@mui/icons-material';
 
 // import { HMIContext } from '../../views/Editor/pages/controls/context';
+export interface HMINodeData {
+    position?: {x: number, y: number};
+    size?: {width: number, height: number};
+    rotation?: number;
+}
 
-export const EditorHandles : React.FC<{id: string, extraProps: any, active: boolean, scaleX: number, scaleY: number, rotation: number, x: number, y: number, children?: any}> = (props) => {
+export interface EditorHandleProps {
+    id: string;
+    extraProps: any;
+    active: boolean;
+    scaleX: number;
+    scaleY: number;
+    
+    width?: number;
+    height?: number;
 
-    const { getRelativeCanvasPos = (opts: {x?: number, y?: number}) => ({x: 0, y: 0}) } = useContext(InfiniteCanvasContext)
+    rotation: number;
+    x: number;
+    y: number;
+    children?: any
+
+    // onEditBounds?: (id: string, updateFn: (node: HMINodeData) => HMINodeData) => void;
+}
+
+export const EditorHandles : React.FC<EditorHandleProps> = (props) => {
+
+    const { getRelativeCanvasPos = (opts: {x?: number, y?: number}) => ({x: 0, y: 0}), updateNodeBounds, updateNode } = useContext(InfiniteCanvasContext)
     // const { updateNode } = useContext(HMIContext)
 
     const corners = [
@@ -79,7 +102,15 @@ export const EditorHandles : React.FC<{id: string, extraProps: any, active: bool
 
                             console.log({theta});
 
-                            // updateNode(props.id, (data) => ({
+                            updateNodeBounds?.(props.id, (node) => {
+                                node.rotation = (Math.floor(theta / 5) * 5) - 90
+                                return node;
+                                // return {
+                                // width,
+                                // height,
+                                // rotation: (Math.floor(theta / 5) * 5) - 90    
+                            })
+                            // (data) => ({
                             //     rotation: (Math.floor(theta / 5) * 5) - 90
                             // }))
 
@@ -110,14 +141,17 @@ export const EditorHandles : React.FC<{id: string, extraProps: any, active: bool
                 {corners.map((corner) => (
                     <div 
                         onPointerDown={(e) => {
-
+                            console.log("Pointer down corner")
                             e.stopPropagation();
+                            e.preventDefault();
 
                             const host = e.currentTarget;
 
                             host.setPointerCapture(e.pointerId)
 
                             let { x: lastX, y: lastY } = getRelativeCanvasPos?.({x: e.clientX, y: e.clientY});
+
+                            const { x, y, width, height } = containerRef.current?.getBoundingClientRect() || {x: 0, y: 0, width: 0, height: 0};
 
                             // containerRef.current
                             
@@ -131,41 +165,68 @@ export const EditorHandles : React.FC<{id: string, extraProps: any, active: bool
 
                                 console.log({newWidth, newHeight});
 
-                                // updateNode(props.id, (node) => {
+                                let position = {x: props.x || 0, y: props.y || 0};
+                                let size = {width: props.width || 0, height: props.height || 0};
+
+
+                                
+                                console.log({position, size, rotation: props.rotation})
+                                // updateNode?.(props.id, {
+                                //     x: position.x,
+                                //     y: position.y
+                                // })
+                                updateNodeBounds?.(props.id, (node) => {
+
+                                    let position = {x: node.x, y: node.y} || {x: 0, y: 0};
+
+                                    let size = {width: node.width, height: node.height} || {width: 0, height: 0};
+    
+                                    if(!size.height) size.height = 0;
+                                    if(!size.width) size.width = 0;
+
+                                    switch(corner.id){
+                                        case 'top-right':
+                                            position.y += newHeight;
+    
+                                            size.width += newWidth;
+                                            size.height += newHeight * -1;
+                                            break;
+                                        case 'bottom-left':
+                                            position.x += newWidth;
+    
+                                            size.width += newWidth * -1;
+                                            size.height += newHeight;
+                                            break;
+                                        case 'top-left':
+                                            position.x += newWidth
+                                            position.y += newHeight;
+    
+                                            size.width += newWidth * -1;
+                                            size.height += newHeight * -1;
+                                            
+                                            break;
+                                        default:
+                                            size.width += newWidth;
+                                            size.height += newHeight;
+                                            break;
+                                    }
+                                  
+                                    node.width = size.width;
+                                    node.height = size.height;
+
+                                    node.x = position.x;
+                                    node.y = position.y;
+                                    
+                                    return node;
+                                });
+
+
+                                // (node) => {
                                 //     console.log("UPDATE", {node})
-                                //     let position = node.position;
-
-                                //     let size = node.size;
-
+                                //   
                                 //     if(!size.width) size.width = 0;
                                 //     if(!size.height) size.height = 0;
 
-                                //     switch(corner.id){
-                                //         case 'top-right':
-                                //             position.y += newHeight;
-
-                                //             size.width += newWidth;
-                                //             size.height += newHeight * -1;
-                                //             break;
-                                //         case 'bottom-left':
-                                //             position.x += newWidth;
-
-                                //             size.width += newWidth * -1;
-                                //             size.height += newHeight;
-                                //             break;
-                                //         case 'top-left':
-                                //             position.x += newWidth
-                                //             position.y += newHeight;
-
-                                //             size.width += newWidth * -1;
-                                //             size.height += newHeight * -1;
-                                            
-                                //             break;
-                                //         default:
-                                //             size.width += newWidth;
-                                //             size.height += newHeight;
-                                //             break;
-                                //     }
 
                                 //     return {
                                 //         position,
@@ -192,6 +253,7 @@ export const EditorHandles : React.FC<{id: string, extraProps: any, active: bool
                             position: 'absolute', 
                             display: !props.active ? 'none' : undefined, 
                             top: corner.top, 
+                            pointerEvents: 'all',
                             left: corner.left, 
                             width: 5, 
                             height: 5, 
