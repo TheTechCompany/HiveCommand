50ae836f8ae1af6eeab26519e4f9c194e67e50bc
Ross Leitch <ross@hexhive.io>
2023-01-27 12:54:46 +1300

    Native build

--- packages/app/hivecommand-native/config-overrides.js
+++ packages/app/hivecommand-native/config-overrides.js
@@ -22,15 +22,24 @@ module.exports = function override(config, env) {
         new TsconfigPathsPlugin()
     )
 
+    config.optimization.usedExports = true;
+
+    // console.log(config.optimization)
+
     // config.plugins.push(new BundleAnalyzerPlugin());
 
     config.resolve.alias = {
         ...config.resolve.alias,
         'react': path.resolve(__dirname, 'node_modules/react'),
+        'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
         // "react/jsx-runtime.js": "react/jsx-runtime",
         // "react/jsx-dev-runtime.js": "react/jsx-dev-runtime",
+        'lodash': path.resolve(__dirname, 'node_modules/lodash'),
+        'moment': path.resolve(__dirname, 'node_modules/moment'),
+        'typescript': path.resolve(__dirname, 'node_modules/typescript'),
         '@hexhive/ui': path.resolve(__dirname, 'node_modules/@hexhive/ui'),
         '@hexhive/utils': path.resolve(__dirname, 'node_modules/@hexhive/utils'),
+        "@mui/x-date-pickers": path.resolve(__dirname, 'node_modules/@mui/x-date-pickers'),
         '@mui/icons-material': path.resolve(__dirname, 'node_modules/@mui/icons-material'),
         '@mui/material': path.resolve(__dirname, 'node_modules/@mui/material'),
         'styled-components': path.resolve(__dirname, 'node_modules/styled-components'),
