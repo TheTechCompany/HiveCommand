b8d44c8918312a9acce2967e1b172deccb072f9c
Ross Leitch <ross@hexhive.io>
2023-02-01 12:40:32 +1300

    Infra public

--- packages/app/hivecommand-frontend/webpack.config.js
+++ packages/app/hivecommand-frontend/webpack.config.js
@@ -33,7 +33,23 @@ module.exports = (webpackConfigEnv, argv) => {
     resolve: {
       alias: {
         'react-resize-aware': path.resolve(__dirname, 'node_modules/react-resize-aware'),
-        '@mui/material': path.resolve(__dirname, 'node_modules/@mui/material')
+        '@mui/material': path.resolve(__dirname, 'node_modules/@mui/material'),
+        "@mui/x-date-pickers": path.resolve(__dirname, 'node_modules/@mui/x-date-pickers'),
+        '@mui/icons-material': path.resolve(__dirname, 'node_modules/@mui/icons-material'),
+
+        'react': path.resolve(__dirname, 'node_modules/react'),
+        'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+        // "react/jsx-runtime.js": "react/jsx-runtime",
+        // "react/jsx-dev-runtime.js": "react/jsx-dev-runtime",
+        'lodash': path.resolve(__dirname, 'node_modules/lodash'),
+        'moment': path.resolve(__dirname, 'node_modules/moment'),
+        'typescript': path.resolve(__dirname, 'node_modules/typescript'),
+        '@hexhive/ui': path.resolve(__dirname, 'node_modules/@hexhive/ui'),
+        '@hexhive/utils': path.resolve(__dirname, 'node_modules/@hexhive/utils'),
+        'styled-components': path.resolve(__dirname, 'node_modules/styled-components'),
+        'react-router-dom': path.resolve(__dirname, 'node_modules/react-router-dom'),
+        '@emotion/react': path.resolve(__dirname, 'node_modules/@emotion/react'),
+        '@hexhive/utils': path.resolve(__dirname, 'node_modules/@hexhive/utils'),
       },
       plugins: [
         new TsconfigPathsPlugin(),
