2ca970309df117dbf6fba32595b34a75894180ad
Ross Leitch <ross@hexhive.io>
2023-02-02 15:24:02 +1300

    Added bit_string to opc

--- packages/communication/opc-ethernet-ip-bridge/src/opc-server/index.ts
+++ packages/communication/opc-ethernet-ip-bridge/src/opc-server/index.ts
@@ -1,96 +1,133 @@
 import OPCServer from '@hive-command/opcua-server'
 import { browseAll, UAObject } from 'node-opcua';
 
-export type TAG_TYPE = 'DINT' | 'INT' | 'REAL';
+export enum TAG_TYPE {
+    BIT_STRING = 'BIT_STRING',
+    STRING = 'STRING',
+    BOOL = 'BOOL',
+    DINT = 'DINT',
+    INT = 'INT',
+    REAL = 'REAL'
+}
 
-export const getOPCType = (type: string) => {
+export enum OPC_TYPE {
+    Boolean = 'Boolean',
+    String = 'String',
+    Number = 'Number',
+    Structure = 'Structure'
+}
+
+function assertUnreachable (x: never): never;
+
+function assertUnreachable (x: TAG_TYPE){
+    throw new Error("Should not be able to reach here: " + x);
+}
+
+export const getOPCType = (type: TAG_TYPE) => {
     switch(type){
-        case 'BOOL':
+
+        case TAG_TYPE.BOOL:
             return 'Boolean';
-        case 'INT':
-        case 'DINT':
-        case 'REAL':
+        case TAG_TYPE.INT:
+        case TAG_TYPE.DINT:
+        case TAG_TYPE.REAL:
             return "Number"
-        default:
-        case 'STRING':
+        case TAG_TYPE.STRING:
             return "String";
+        case TAG_TYPE.BIT_STRING:
+            return '[Boolean]'
+        default:
+            return assertUnreachable(type);
     }
+    
 }
 
 export const addTag = async (
     server: OPCServer, 
     tagname: string, 
-    type: string, 
+    type: TAG_TYPE, 
     getter: (() => any | undefined),
     setter: (value: any, key?: string) => void,
-    structure?: {[key: string]: string}, parent?: UAObject
+    structure?: {[key: string]: TAG_TYPE}, parent?: UAObject
 ) => {
 
-    let dataType : 'Boolean'  | 'Structure' | 'String' | 'Number' = 'String';
+    let dataType : OPC_TYPE = OPC_TYPE.String;
 
     console.log(`Add tag ${tagname}:${type} struct:${structure && type !== 'STRING'}`);
 
     if(structure){
         console.log(`Tag structure`, JSON.stringify(structure, null, 2));
     }
-    switch((structure && type !== 'STRING') ? 'STRUCT' : type){
-        case 'STRUCT':
-            dataType = 'Structure';
 
-            const rootObject = await server.addObject(tagname, parent)
+    if(structure && type !== TAG_TYPE.STRING){
+        dataType = OPC_TYPE.Structure;
 
-            await Promise.all(Object.keys(structure || {}).map(async (key) => {
+        const rootObject = await server.addObject(tagname, parent)
 
-                let defaultValue: any;
+        await Promise.all(Object.keys(structure || {}).map(async (key) => {
 
-                if(structure?.[key]){
-                    switch(getOPCType(structure?.[key])){
-                        case 'Boolean':
-                            defaultValue = false;
-                            break;
-                        case 'Number':
-                            defaultValue = 0;
-                            break;
-                        case 'String':
-                            defaultValue = '';
-                            break;
-                    }
+            let defaultValue: any;
 
-                    await addTag(server, key, structure?.[key] || '', () => {
-                        return getter()?.[key] || defaultValue
-                    }, (value) => setter(value, key), undefined, rootObject);
+            if(structure?.[key]){
+                switch(getOPCType(structure?.[key] as TAG_TYPE)){
+                    case '[Boolean]':
+                        defaultValue = [];
+                        break;
+                    case 'Boolean':
+                        defaultValue = false;
+                        break;
+                    case 'Number':
+                        defaultValue = 0;
+                        break;
+                    case 'String':
+                        defaultValue = '';
+                        break;
                 }
 
-                // await server.addVariable(key, getOPCType(structure?.[key] || ''), )
-            }));
+                await addTag(server, key, structure?.[key], () => {
+                    return getter()?.[key] || defaultValue
+                }, (value) => setter(value, key), undefined, rootObject);
+            }
 
-            break;
-        case 'BOOL':
-            dataType = 'Boolean'
-            break;
-        case 'INT':
-        case 'DINT':
-        case 'REAL':
-            dataType = "Number"
-            break;
-        default:
-        case 'STRING':
-            dataType = "String";
-            break;
+            // await server.addVariable(key, getOPCType(structure?.[key] || ''), )
+        }));
+
+    }else{
+        switch(type){
+           
+            case TAG_TYPE.BIT_STRING:
+            case TAG_TYPE.BOOL:
+                dataType = OPC_TYPE.Boolean;
+                break;
+            case TAG_TYPE.INT:
+            case TAG_TYPE.DINT:
+            case TAG_TYPE.REAL:
+                dataType = OPC_TYPE.Number;
+                break;
+            case TAG_TYPE.STRING:
+                dataType = OPC_TYPE.String;
+                break;
+            default: 
+                return assertUnreachable(type)
+        }
     }
 
     let defaultValue: any;
 
     switch(dataType){
-        case 'Boolean':
+        case OPC_TYPE.Boolean:
             defaultValue = false;
             break;
-        case 'Number':
+        case OPC_TYPE.Number:
             defaultValue = 0;
             break;
-        case 'String':
+        case OPC_TYPE.String:
             defaultValue = '';
             break;
+        case OPC_TYPE.Structure:
+            break;
+        default:
+            return assertUnreachable(dataType)
     }
 
     const _getter = () => {
@@ -100,7 +137,7 @@ export const addTag = async (
 
     // }
 
-    if(dataType != 'Structure'){
+    if(dataType != OPC_TYPE.Structure){
         await server.addVariable(tagname, dataType, _getter, setter, parent)
     }
 }
