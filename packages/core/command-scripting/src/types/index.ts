8945a34a33a01bbc6bca06109f86eb5a80706687
Ross Leitch <ross@hexhive.io>
2023-02-03 13:54:18 +1300

    DRYing the @hive-command/scripting package

---
+++ packages/core/command-scripting/src/types/index.ts
@@ -0,0 +1,14 @@
+export * from './enip-plc';
+export * from './opc';
+
+export enum DataTypes {
+    Boolean = 'boolean',
+    String = 'string',
+    Number = 'number',
+    Date = 'Date'
+    // Structure = 'Structure'
+}
+
+export const lookupType = (type: keyof typeof DataTypes) => {
+    return DataTypes[type]
+}
