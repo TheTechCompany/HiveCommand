65d52d3bb690d20a1ffb3aebf25ba0be6b6ebd62
Ross Leitch <ross@hexhive.io>
2023-09-14 20:56:11 +1200

    Find viewport issue

--- packages/core/command-schematic-export/src/index.ts
+++ packages/core/command-schematic-export/src/index.ts
@@ -114,35 +114,37 @@ export const export_schematic = async (schematic: {name: string, pages: any[]}, 
 
 }
 
-export_schematic({
-    name: 'Projec',
-    pages: [
-        {
-            id: '1', 
-            nodes: [
-                {id: '1', type: 'electricalSymbol', position: {x: 50, y: 10}, data: {} }, 
-                {id: '2', type: 'electricalSymbol', position: {x:  1200, y: 800}, data: {symbol: 'AcCoil'} 
-            }
-        ],
-            edges: [{id: '2', type: 'wire', source: 'canvas', target: 'canvas', points: [{x: 50, y: 50}, {x: 100, y: 100}] }]
-        },
-        {id: '2', nodes: [{ id: '2', type: 'electricalSymbol', position: {x: 10, y: 10}, data: {symbol: 'AcCoil'} }]},
-    ]
-}, {
-    executablePath: '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome',
-    defaultViewport: {
-        // "deviceScaleFactor":1,"hasTouch":false,
-        "height":1080,
-        // "isLandscape":true,"isMobile":false,
-        "width":1920
-    },
-    headless: false,
-    // args: ["--allow-running-insecure-content","--autoplay-policy=user-gesture-required","--disable-background-timer-throttling","--disable-component-update","--disable-domain-reliability","--disable-features=AudioServiceOutOfProcess,IsolateOrigins,site-per-process","--disable-ipc-flooding-protection","--disable-print-preview","--disable-dev-shm-usage","--disable-setuid-sandbox","--disable-site-isolation-trials","--disable-speech-api","--disable-web-security","--disk-cache-size=33554432","--enable-features=SharedArrayBuffer","--hide-scrollbars","--ignore-gpu-blocklist","--in-process-gpu","--mute-audio","--no-default-browser-check","--no-first-run","--no-pings","--no-sandbox","--no-zygote","--use-gl=angle","--use-angle=swiftshader","--window-size=1920,1080","--single-process"]
-    // viewport: {
-    //     width: 600,
-    //     height: 800
-    // }
-}).then((pdf: any) => {
-        writeFileSync('./test.pdf', pdf)
 
-})
+
+// export_schematic({
+//     name: 'Projec',
+//     pages: [
+//         {
+//             id: '1', 
+//             nodes: [
+//                 {id: '1', type: 'electricalSymbol', position: {x: 50, y: 10}, data: {} }, 
+//                 {id: '2', type: 'electricalSymbol', position: {x:  1200, y: 800}, data: {symbol: 'AcCoil'} 
+//             }
+//         ],
+//             edges: [{id: '2', type: 'wire', source: 'canvas', target: 'canvas', points: [{x: 50, y: 50}, {x: 100, y: 100}] }]
+//         },
+//         {id: '2', nodes: [{ id: '2', type: 'electricalSymbol', position: {x: 10, y: 10}, data: {symbol: 'AcCoil'} }]},
+//     ]
+// }, {
+//     executablePath: '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome',
+//     defaultViewport: {
+//         // "deviceScaleFactor":1,"hasTouch":false,
+//         "height":1080,
+//         // "isLandscape":true,"isMobile":false,
+//         "width":1920
+//     },
+//     headless: false,
+//     // args: ["--allow-running-insecure-content","--autoplay-policy=user-gesture-required","--disable-background-timer-throttling","--disable-component-update","--disable-domain-reliability","--disable-features=AudioServiceOutOfProcess,IsolateOrigins,site-per-process","--disable-ipc-flooding-protection","--disable-print-preview","--disable-dev-shm-usage","--disable-setuid-sandbox","--disable-site-isolation-trials","--disable-speech-api","--disable-web-security","--disk-cache-size=33554432","--enable-features=SharedArrayBuffer","--hide-scrollbars","--ignore-gpu-blocklist","--in-process-gpu","--mute-audio","--no-default-browser-check","--no-first-run","--no-pings","--no-sandbox","--no-zygote","--use-gl=angle","--use-angle=swiftshader","--window-size=1920,1080","--single-process"]
+//     // viewport: {
+//     //     width: 600,
+//     //     height: 800
+//     // }
+// }).then((pdf: any) => {
+//         writeFileSync('./test.pdf', pdf)
+
+// })
