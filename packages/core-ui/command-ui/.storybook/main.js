9cab44eaf65a59c4e14208567c439f5c40a2c054
Ross Leitch <30274587+balbatross@users.noreply.github.com>
2023-09-22 07:48:36 +0000

    Core-ui stories

--- packages/core-ui/command-ui/.storybook/main.js
+++ packages/core-ui/command-ui/.storybook/main.js
@@ -1,15 +1,15 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
-
 import { join, dirname } from "path";
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string): any {
+function getAbsolutePath(value) {
   return dirname(require.resolve(join(value, "package.json")));
 }
-const config: StorybookConfig = {
+
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
   stories: [
     "../stories/**/*.mdx",
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
