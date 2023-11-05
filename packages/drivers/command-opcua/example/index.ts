60a5a864a80350fdc27808393378b8f78edb309d
Ross Leitch <ross@hexhive.io>
2023-11-06 10:30:19 +1300

    Updated opcua version, added first implementation of opcua driver

--- packages/drivers/command-opcua/example/index.ts
+++ packages/drivers/command-opcua/example/index.ts
@@ -8,7 +8,7 @@ import OPCUAClient from '@hive-command/opcua-client'
 
     console.log("Conenctec!")
     
-    const dv = await client.getDetails(`/Objects/Server/AC500 PM56xx-2ETH/Resources/Application/GlobalVars/GVL/AV201/Eng`)
+    const dv = await client.getDetails(`/Objects/0:Server/4:AC500 PM56xx-2ETH/3:Resources/4:Application/3:GlobalVars/4:GVL/4:AV201/4:Eng`)
 
     console.log(dv);
 })();
--- packages/drivers/command-opcua/src/index.ts
+++ packages/drivers/command-opcua/src/index.ts
@@ -8,11 +8,13 @@ export default class OPCUADriver extends BaseCommandDriver {
     private client : OPCUAClient;
 
     private basePath : string;
+    private namespace: string;
 
     constructor(options: DriverOptions){
         super(options);
         this.client = new OPCUAClient();
 
+        this.namespace = options.configuration?.namespace;
         this.basePath = options.configuration?.basePath;
     }
 
@@ -20,26 +22,62 @@ export default class OPCUADriver extends BaseCommandDriver {
         await this.client.connect(this.options.configuration?.endpoint)
     }
 
+    async stop(){
+        await this.client.disconnect()
+    }
+
     async subscribe(tags: { name: string; alias?: string | undefined; }[]): Promise<Observable<{ [key: string]: any; }>> {
         return await new Promise(async (resolve) => {
 
-            const { monitors, unsubscribe, unwrap } = await this.client.subscribeMulti([], 500)
+            const subscribeTags = tags.map((tag) => {
+                let parts = tag.name.split('.');
+
+                if(parts.length > 1){
+                    return {
+                        path: `${this.basePath}/${parts.map((x) => `${this.namespace}:${x}`).join('/')}`,
+                        tag: tag.name
+                    }
+                }else{
+                    return {
+                        path: `${this.basePath}/${this.namespace}:${tag.name}`,
+                        tag: tag.name
+                    }
+                }
+            });
+
+            const { monitors, unsubscribe, unwrap } = await this.client.subscribeMulti(subscribeTags, 500)
 
             const subject = new Subject<{[key: string]: any}>();
 
+            monitors?.on('changed', (monitoredItem, dv, index) => {
+                const key = unwrap(index);
+                const value = dv.value.value;
+
+                subject.next({[key]: value});
+            })
 
             resolve(Observable.from(subject));
-    
         })
 
     }
 
     async read(tag: { name: string; alias: string; }) {
-        const dv = await this.client.getDetails(tag.name)
+        const path = `${this.basePath}/${this.namespace}:${tag.name}`;
+
+        const dv = await this.client.getDetails(path)
         return dv?.value.value;
     }
 
     async write(tag: string, value: any) {
-        await this.client.setDetails(tag, DataType.Boolean, value);
+        const tag_parts = tag.split('.');
+        
+        let path = '';
+        if(tag_parts.length > 1){
+            path = `${this.basePath}/${tag_parts.map((x) => `${this.namespace}:${x}`).join('/')}`
+        }else{
+            path = `${this.basePath}/${this.namespace}:${tag}`
+        }
+
+        await this.client.setDetails(path, DataType.Boolean, value);
     }
 }
